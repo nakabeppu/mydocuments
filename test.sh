8297e4fcf993968b6caa55d0f67facce07b31b2a
nakabeppu <taichi.nakabeppu@kikagaku.co.jp>
2022-07-07 17:25:27 +0900

    fix

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 make clean
 make html
-mkdir -p ./docs
-rm -r ./docs/*
+rm -r ./docs
+mkdir ./docs
 cp -rp _build/html/* ./docs
+cd ./docs
+touch .nojekyll
 # git add .
 # git status
 # git commit -m "shellによるプッシュ"
 # git status
+cd ../
 open _build/html/index.html
